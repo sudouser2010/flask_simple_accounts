d8d2d1e9270168e7ebe3fd552512342ddfa79c37
herbert <hd>
2017-11-12 22:34:29 -0500

    Modified setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'flask_simple_accounts',
   packages = ['flask_simple_accounts'],
-  version = '1.0',
+  version = '1.11',
   description = 'This library allows Flask developers to use the an api to handle the mundane tasks of user account management',
   author = 'Herbert Dawkins',
   author_email = 'DrDawkins@ClearScienceInc.com',
@@ -11,5 +11,9 @@ setup(
   keywords = ['flask', 'simple', 'accounts'],
   classifiers = [],
   python_requires='~=3.6',
-  install_requires=['flask_multiple_static_folders', 'flask_beautiful_messages', 'flask_render_specific_template'],
+  install_requires=[
+  'flask_multiple_static_folders', 'flask_beautiful_messages', 'flask_render_specific_template',
+  'SQLAlchemy==1.1.10', 'bcrypt==3.1.3', 'Flask-Mail==0.9.1',
+  'flask_login'
+  ],
 )
