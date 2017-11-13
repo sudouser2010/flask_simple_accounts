04eff8704c40f7c8fe84d10ba3db30c08d7a9924
herbert <hd>
2017-11-12 22:57:19 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'flask_simple_accounts',
   packages = ['flask_simple_accounts'],
-  version = '1.12',
+  version = '1.13',
   description = 'This library allows Flask developers to use the an api to handle the mundane tasks of user account management',
   author = 'Herbert Dawkins',
   author_email = 'DrDawkins@ClearScienceInc.com',
